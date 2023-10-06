5bd271b8e04c66b5955953e75dec3b05a69ec6a0
Giancarlo Cabrera <giancarlo.cabrera@unac.edu.co>
2023-10-06 14:52:09 -0500

    Integrating TypeORM and User Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,9 @@ import { AuthModule } from 'src/auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { APP_GUARD } from '@nestjs/core';
 import { ApiKeyGuard } from './auth/apiKeyAuth/apikey.guard';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+import User from './users/user.entity';
 
 @Module({
   imports: [
@@ -12,7 +15,21 @@ import { ApiKeyGuard } from './auth/apiKeyAuth/apikey.guard';
       envFilePath: '.env',
       isGlobal: true,
     }),
-    AuthModule
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5433,
+      username: 'postgres',
+      password: 'GIAN_PG_ADMIN_DB_PS',
+      database: 'GC-store',
+      // Read all entities inside src
+      // entities: [__dirname + '/**/*.entity{.ts,.js'],
+      entities: [User],
+      // Not recommended to use on prod, this is to sync db tables with ptoject tables
+      synchronize: true,
+    }),
+    AuthModule,
+    UsersModule
   ],
   controllers: [AppController],
   providers: [
---
+++ src/users/user.entity.ts
@@ -0,0 +1,22 @@
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm"
+
+@Entity()
+export default class User {
+  @PrimaryGeneratedColumn()
+  id: number
+
+  @Column({ unique: true })
+  username: string
+
+  @Column()
+  password: string
+
+  @CreateDateColumn({ type: "timestamp", default: () => "CURRENT_TIMESTAMP(6)" })
+  createdAt: Date
+
+  @UpdateDateColumn({ type: "timestamp", default: () => "CURRENT_TIMESTAMP(6)", onUpdate: "CURRENT_TIMESTAMP(6)" })
+  updated_at: Date;
+
+  @Column({ nullable: true })
+  authStrategy: string
+}
